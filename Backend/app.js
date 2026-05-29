ad0644dbd54f29e9c23346db073fe7d142d7aec7
harshit <harshit.merdev@gmail.com>
2026-05-29 09:17:01 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,12 @@
+const dotenv=require("dotenv");
+const cors=require("cors");
+dotenv.config();
+const express =require("express");
+const app=express();
+app.use(cors());
+
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+module.exports=app;
